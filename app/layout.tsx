e857135551fe0c808dcd23fd0dfaa32741636254
Amith B V <amithrao0606@gmail.com>
2025-06-10 13:32:09 +0530

    Removed the head tag.

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,13 +29,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        {/* Fix the Google Font error */}
-        <link
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
